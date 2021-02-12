13266894566ae3d72d67df59cd8d8c89f562b85a
adityajv2310 <jvaditya@gmail.com>
2021-02-12 13:24:18 +0000

    The multiplication table code is completed

--- multiplicationtable.py
+++ multiplicationtable.py
@@ -1,23 +1,3 @@
-# Developing a Simple Webserver
-## AIM:
-To develop a simple webserver to serve html pages.
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation
-### Step 2:
-Design of webserver workflow
-### Step 3:
-Implementation using Python code
-### Step 4:
-Serving the HTML pages.
-### Step 5:
-Testing the webserver
-
-## PROGRAM:
-
-### multiplicationtable.py
-~~~
 from http.server import HTTPServer, BaseHTTPRequestHandler
 content = """
 <!DOCTYPE html>
@@ -52,10 +32,3 @@ server_address = ('',80)
 httpd = HTTPServer(server_address,MyHandler)
 print("my Webserver is running...")
 httpd.serve_forever()
-~~~
-
-## OUTPUT:
-![output](./static/img/m1.png)
-
-## RESULT:
-Thus we have developed a webserver to display the multiplication table of 11 and it is hosted in the url http://adityajv.student.saveetha.in/. GitHub Repo URL is https://github.com/adityajv2310/simplewebserver.
